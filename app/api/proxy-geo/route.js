8c8b701d41034c8e3aa89cba4e0b25a81fc196ca
3DravecAI <postz@rocketmail.com>
2026-04-12 21:56:26 +0100

    Add proxy for geo location

--- app/api/proxy-geo/route.js
+++ app/api/proxy-geo/route.js
@@ -1,9 +1,20 @@
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
+export async function GET(request) {
   try {
-    const response = await fetch('http://ip-api.com/json/');
+    // Вземаме реалния IP на потребителя от хедърите на заявката
+    const forwardedFor = request.headers.get('x-forwarded-for');
+    const realIp = forwardedFor ? forwardedFor.split(',')[0] : request.headers.get('x-real-ip');
+    
+    console.log('📡 Реален IP на потребителя:', realIp);
+    
+    // Изпращаме IP адреса към ip-api.com
+    const apiUrl = realIp ? `http://ip-api.com/json/${realIp}` : 'http://ip-api.com/json/';
+    const response = await fetch(apiUrl);
     const data = await response.json();
+    
+    console.log('📍 Локация от ip-api.com:', data);
+    
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
